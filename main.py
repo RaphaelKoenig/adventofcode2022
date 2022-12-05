6627d290916699638fa9027a3f4c3262a592ef73
Raphael <raphael_koenig@gmx.de>
2022-12-05 11:13:13 +0100

    Solutions day 5

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from day01.day01 import solve_day01
 from day02.day02 import solve_day02
 from day03.day03 import solve_day03
 from day04.day04 import solve_day04
+from day05.day05 import solve_day05
 
 
 def solve_all():
-    solve_day01()  # solution (1): 66186 (1.06 ms) | (2): 196804 (0.99 ms)
-    solve_day02()  # solution (1): 14264 (1.73 ms) | (2):  12382 (1.66 ms) #optimized to 0.85 ms
-    solve_day03()  # solution (1):  8394 (0.92 ms) | (2):   2413 (0.72 ms)
-    solve_day04()  # solution (1):  567  (2.01 ms) | (2):    907 (2.62 ms)
+    solve_day01()  # solution (1):      66186 (1.06 ms) | (2):    196804 (0.99 ms)
+    solve_day02()  # solution (1):      14264 (0.85 ms) | (2):     12382 (0.85 ms) # optimized from 1.75 ms
+    solve_day03()  # solution (1):       8394 (0.92 ms) | (2):      2413 (0.72 ms)
+    solve_day04()  # solution (1):        567 (2.01 ms) | (2):       907 (2.62 ms)
+    solve_day05()  # solution (1):  FCVRLMVQP (2.04 ms) | (2): RWLWGJGFD (2.08 ms) # optimized to 1.5 ms
 
 
 if __name__ == '__main__':
