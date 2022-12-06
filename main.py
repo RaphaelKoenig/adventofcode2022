26668138deb8f1c2b6c2d70c74dd666ea1076c09
Raphael <raphael_koenig@gmx.de>
2022-12-06 10:19:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from day06.day06 import solve_day06
 
 def solve_all():
     solve_day01()  # solution (1):      66186 (1.06 ms) | (2):    196804 (0.99 ms)
-    solve_day02()  # solution (1):      14264 (0.85 ms) | (2):     12382 (0.85 ms) # optimized from 1.75 ms
+    solve_day02()  # solution (1):      14264 (0.85 ms) | (2):     12382 (0.85 ms) #      optimized from 1.75 ms
     solve_day03()  # solution (1):       8394 (0.92 ms) | (2):      2413 (0.72 ms)
     solve_day04()  # solution (1):        567 (2.01 ms) | (2):       907 (2.62 ms)
-    solve_day05()  # solution (1):  FCVRLMVQP (2.04 ms) | (2): RWLWGJGFD (2.08 ms) # optimized to 1.5 ms
-    solve_day06()
+    solve_day05()  # solution (1):  FCVRLMVQP (2.04 ms) | (2): RWLWGJGFD (2.08 ms) # both optimized to    1.5 ms
+    solve_day06()  # solution (1):       1578 (1.31 ms) | (2):      2178 (0.43 ms) # (2)  optimized from 1.56 ms
 
 
 if __name__ == '__main__':
