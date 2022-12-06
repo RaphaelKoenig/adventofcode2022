ee70691578f436403312af1c11fbcadd16c8ea92
Raphael <raphael_koenig@gmx.de>
2022-12-06 10:14:43 +0100

    Solutions day 6

---
+++ day06/day06.py
@@ -0,0 +1,105 @@
+# template day
+import time
+
+day_str = "06"
+
+
+def solve_day06():
+    solve_day06_1()
+    solve_day06_2()
+
+
+def solve_day06_1():
+
+    # start execution time
+    start_time = time.perf_counter()
+
+    # read input file
+    input_file = open('day' + day_str + '/input.txt')
+    lines = input_file.readlines()
+    # print(lines)
+
+    # remove \n and whitespaces
+    lines = [x.strip() for x in lines]
+
+    # define variables
+    characters = lines[0]
+    # start of paket marker = spm
+    spm_length = 4
+
+    # solve
+    spm_index = calculate_spm_index(characters, spm_length)
+
+    for i in range(0, len(characters)-3):
+        if len(set(characters[i:i+4])) == 4:
+            start_of_paket_marker_index = i + 4
+            break
+
+    # result
+    result = spm_index
+
+    # stop execution time
+    end_time = time.perf_counter()
+
+    print('Day {} (1) solution: {} (execution time: {} ms)'.format(day_str, result, round((end_time - start_time) * 1000, 2)))
+
+
+def solve_day06_2():
+    # start execution time
+    start_time = time.perf_counter()
+
+    # read input file
+    input_file = open('day' + day_str + '/input.txt')
+    lines = input_file.readlines()
+    # print(lines)
+
+    # remove \n and whitespaces
+    lines = [x.strip() for x in lines]
+
+    # define variables
+    characters = lines[0]
+    # start of paket marker = spm
+    spm_length = 14
+
+    # solve
+    spm_index = calculate_spm_index_optimized(characters, spm_length)
+
+    # result
+    result = spm_index
+
+    # stop execution time
+    end_time = time.perf_counter()
+
+    print('Day {} (2) solution: {} (execution time: {} ms)'.format(day_str, result, round((end_time - start_time) * 1000, 2)))
+
+
+# loop through characters and check if there is a sequence of characters with length = spm_length
+def calculate_spm_index(characters, spm_length):
+
+    for i in range(0, len(characters)-spm_length+1):
+        distinct_characters = len(set(characters[i:i+spm_length]))
+        if distinct_characters == spm_length:
+            spm_index = i + spm_length
+            return spm_index
+
+
+# Use knowledge about the amount of distinct characters to skip loops
+# Example: spm_length = 14, distinct characters 10: Skip 3 loops
+# because there cannot be sequence of 14 distinct characters in the next 3 loops because it needs at least these 3 loops
+# to remove the duplicates
+
+# (1) 1.31 -> 1.42 ms ( 101 loops skipped) 1575 -> 1474 ( 6% reduction of loops) but slower because of offset calculation
+# (2) 1.56 -> 0.43 ms (1782 loops skipped) 2165 ->  383 (82% reduction of loops)
+def calculate_spm_index_optimized(characters, spm_length):
+
+    offset = 0
+    for i in range(0, len(characters)-spm_length+1):
+        i += offset
+        distinct_characters = len(set(characters[i:i+spm_length]))
+        if distinct_characters == spm_length:
+            spm_index = i + spm_length
+            return spm_index
+        offset += spm_length-distinct_characters-1
+
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from day02.day02 import solve_day02
 from day03.day03 import solve_day03
 from day04.day04 import solve_day04
 from day05.day05 import solve_day05
+from day06.day06 import solve_day06
 
 
 def solve_all():
@@ -11,6 +12,7 @@ def solve_all():
     solve_day03()  # solution (1):       8394 (0.92 ms) | (2):      2413 (0.72 ms)
     solve_day04()  # solution (1):        567 (2.01 ms) | (2):       907 (2.62 ms)
     solve_day05()  # solution (1):  FCVRLMVQP (2.04 ms) | (2): RWLWGJGFD (2.08 ms) # optimized to 1.5 ms
+    solve_day06()
 
 
 if __name__ == '__main__':
